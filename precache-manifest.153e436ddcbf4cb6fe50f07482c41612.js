ce6b2102da05b244ff89df1f70ff5bcd3bd1d4b8
girish5f9@sasi.ac.in <girish5f9@sasi.ac.in>
2020-11-12 00:54:49 +0530

    Updates

---
+++ precache-manifest.153e436ddcbf4cb6fe50f07482c41612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e01b43fa823d4cdc14105ba44d2efc",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "f7b9fc2225e281b3ac1b",
+    "url": "/weatherapp/static/css/main.d1cdf409.chunk.css"
+  },
+  {
+    "revision": "bc27ce3258032095663f",
+    "url": "/weatherapp/static/js/2.e1bb8c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.e1bb8c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b9fc2225e281b3ac1b",
+    "url": "/weatherapp/static/js/main.49fc83ad.chunk.js"
+  },
+  {
+    "revision": "62221dd54313b54c4698",
+    "url": "/weatherapp/static/js/runtime-main.bfce4d0d.js"
+  }
+]);
